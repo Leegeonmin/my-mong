7d6953bc0a6f02fac05375d3d3174bcf4b0cf2b4
Leegeonmin <kmlee97s@gmail.com>
2025-07-06 23:45:43 +0900

    feat : SecurityConfig 추가

---
+++ src/main/java/com/lgm/my_mong/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.lgm.my_mong.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+
+                // 2. 세션 사용 안함 (JWT는 Stateless)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // 3. URL별 권한 설정
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("v1/api/health/**").permitAll()     // 로그인/회원가입 허용
+                        .anyRequest().authenticated()                    // 나머지는 인증 필요
+                )
+
+                // 4. JWT 필터를 Spring Security 필터 체인에 추가
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
