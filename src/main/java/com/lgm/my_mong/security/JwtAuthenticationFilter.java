85955457e56c00223a9d866b98aec6cceaedb4c2
Leegeonmin <kmlee97s@gmail.com>
2025-07-06 22:50:03 +0900

    feat : JwtAuthenticationFilter 추가
    사용자 인증 후 정보 넘기는 로직 추가

---
+++ src/main/java/com/lgm/my_mong/security/JwtAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package com.lgm.my_mong.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = extractTokenFromHeader(request);
+
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            String username = jwtTokenProvider.getUsername(token);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    username,           // 사용자명
+                    null,              // 비밀번호 (이미 검증했으니 null)
+                    new ArrayList<>()   // 권한 (일단 빈 리스트)
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractTokenFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7); // "Bearer " 제거
+        }
+        return null;
+    }
+}
